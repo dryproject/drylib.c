39866df89ae4f97f0a39aa11db0a403280dc6484
Arto Bendiken <arto@dryproject.org>
2017-09-04 15:26:43 +0200

    Defined the test runner.

---
+++ test.c
@@ -0,0 +1,11 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include "drylib.h"
+
+#include <stdlib.h> /* for EXIT_SUCCESS */
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[]) {
+  return EXIT_SUCCESS; // TODO
+}
