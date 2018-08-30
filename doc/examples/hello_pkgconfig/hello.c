e26823649b7e6ec6b84135d8b915a1d171e58614
Arto Bendiken <arto@dryproject.org>
2018-08-30 18:03:44 +0300

    Added a "Hello, world!" example for pkg-config.

---
+++ doc/examples/hello_pkgconfig/hello.c
@@ -0,0 +1,10 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <drylib.h>
+
+#include <stdlib.h> /* for EXIT_SUCCESS */
+
+int main(int argc, char* argv[]) {
+  // TODO: print "Hello, world!\n"
+  return EXIT_SUCCESS;
+}
